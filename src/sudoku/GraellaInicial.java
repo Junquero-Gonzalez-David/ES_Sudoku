efcd2109d17f06e03002298d5e504896658a15f2
David Junquero González <djunquero@edu.tecnocampus.cat>
2016-09-30 11:24:59 +0200

    Add files via upload

--- src/sudoku/GraellaInicial.java
+++ src/sudoku/GraellaInicial.java
@@ -3,23 +3,29 @@ package sudoku;
 public class GraellaInicial {
 
 	int numeros[][];
-	
-	public String toString(){
-		
-		//TODO Check the table is displayed properly
-		
-		String output="";
-		for(int i=0;i<numeros.length;i++){
-			for(int j=0;j<numeros[i].length;i++){
-				output+=numeros[i][j]+'\t';
-				if(j%3==0) output+=" | "; // Could not be correct
+
+	public String toString() {
+
+		// TODO Check the table is displayed properly
+
+		String output = "";
+		for (int i = 0; i < numeros.length; i++) {
+			for (int j = 0; j < numeros[i].length; i++) {
+				output += numeros[i][j] + '\t';
+				if (j % 3 == 0)
+					output += " | "; // Could not be correct
 			}
 		}
-		
+
 		return output;
 	}
-	
-	public boolean isComplete(){
-		return true; // TODO Complete
+
+	public boolean isComplete() {
+		for (int i = 0; i < numeros.length; i++) {
+			for (int j = 0; j < numeros[i].length; i++) {
+				if (numeros[i][j] == this.BLANC)
+					return false;
+			}
+		} return true;
 	}
 }
