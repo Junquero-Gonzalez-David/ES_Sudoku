506210d2b7bd4c489a7387afa495bf17db2aa67e
Jan Jiménez Serra <jan.jime.serra@gmail.com>
2016-09-30 13:17:48 +0200

    Update GraellaInicial.java

--- src/sudoku/GraellaInicial.java
+++ src/sudoku/GraellaInicial.java
@@ -1,31 +1,55 @@
 package sudoku;
 
 public class GraellaInicial {
-
-	int numeros[][];
-
-	public String toString() {
-
-		// TODO Check the table is displayed properly
-
-		String output = "";
-		for (int i = 0; i < numeros.length; i++) {
-			for (int j = 0; j < numeros[i].length; i++) {
-				output += numeros[i][j] + '\t';
-				if (j % 3 == 0)
-					output += " | "; // Could not be correct
+	private char [][]graella;
+	private int correctes;
+	private static final char BLANC = '?'; 
+	
+	public GraellaInicial() {
+		graella = new char[9][9];
+		correctes = 17;
+	}
+	
+	public boolean afegirNumero(int fila, int col, int num){
+		if(this.comprovar(fila, col, num)){
+			//afegir a graella
+			return true;
+		}
+		else return false;
+	}
+	public boolean comprovar(int fila, int col, int num){
+		return true;
+	}
+	public void iniciar(){
+		for (int i = 0; i < graella.length; i++) {
+			for (int j = 0; j < graella[i].length; j++) {
+				this.graella[i][j] = BLANC;
 			}
 		}
-
+	}
+	public String toString(){
+		// TODO Check the table is displayed properly
+		String output = "";
+		for (int i = 0; i < graella.length; i++) {
+			for (int j = 0; j < graella[i].length; j++) {
+				output += graella[i][j] + "   ";
+				if (j==2||j==5){
+					output += "|   ";
+				}
+			}
+			output += '\n';
+			if (i==2||i==5) output += "-----------------------------------------";
+			output += '\n';
+		}
 		return output;
 	}
 
 	public boolean isComplete() {
-		for (int i = 0; i < numeros.length; i++) {
-			for (int j = 0; j < numeros[i].length; i++) {
-				if (numeros[i][j] == this.BLANC)
+		for (int i = 0; i < graella.length; i++) {
+			for (int j = 0; j < graella[i].length; i++) {
+				if (graella[i][j] == BLANC)
 					return false;
 			}
-		} return true;
-	}
+	} return true;
+}
 }
